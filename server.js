80b1aab32ee18bef9c4898c3af1fbc547714744d
ShivaliWaghmare <113419537+ShivaliWaghmare@users.noreply.github.com>
2024-03-09 19:17:08 +0530

    database connected

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const cors = require("cors")
+const morgan = require("morgan")
+const dotenv = require("dotenv")
+const colors = require("colors")
+const connectDb = require("./config/connectDb")
+
+// Config dot env file
+dotenv.config();
+
+// Database Call
+connectDb();
+
+// Rest object
+const app = express()
+
+// Middlewares
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(cors())
+
+// Routes
+app.get( '/', (req, res) => {
+    res.send("<h1>Hello from Server</h1>");
+});
+
+// Port
+const PORT = 8080 || process.env.PORT
+
+// Listen Server
+app.listen( PORT, ()=>{
+    console.log(`Server running on port ${PORT}`);
+});
