0e5c765003f904c473d2feb9db55e66564918a38
ShivaliWaghmare <113419537+ShivaliWaghmare@users.noreply.github.com>
2024-03-29 20:35:48 +0530

    Created componentes

---
+++ client/src/components/Layout/footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+    <div className='bg-dark text-light'>
+       <h6 className='text-center'>All right reserved &copy; shivali</h6>
+    </div>
+  )
+}
+
+export default footer
