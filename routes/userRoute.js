000443d8bb379bb6b315bd5d0499e15ffe04ab70
ShivaliWaghmare <113419537+ShivaliWaghmare@users.noreply.github.com>
2024-03-29 13:39:10 +0530

    Created userRoute.js and userController.js

---
+++ routes/userRoute.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const { loginControllers, registerControllers } = require('../controllers/userControllers')
+
+// Router Object : To preform routing
+const router = express.Router()
+
+// Routers :
+
+// POST || LOGIN USER
+router.post('/login', loginControllers)
+
+// POST || REGISTER USER
+router.post('/register', registerControllers)
+
+// Export:
+module.exports = router
