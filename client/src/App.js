1b330d4011f9c1b71c0154d595703268b1621b06
ShivaliWaghmare <113419537+ShivaliWaghmare@users.noreply.github.com>
2024-03-30 20:03:48 +0530

    Updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import {Routes, Route} from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div className='text-center'>Shivali here.</div>
+    <>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+
+const HomePage = () => {
+  return (
+    <Layout>
+       <h6>Home Page</h6>
+    </Layout>
+  )
+}
+
+export default HomePage
